54b018f6e9c03279df1fde0265eff523090c5a79
AdrianCielos <47727514+AdrianCielos@users.noreply.github.com>
2019-07-09 13:17:34 +0800

    Update Experiment2.4.cpp

--- Experiment2.4.cpp
+++ Experiment2.4.cpp
@@ -10,16 +10,21 @@ int main ()
 	int x;
 	cout << "Counting..." << endl;
 	
+	//ARITHMETIC TO OUTPUT COUNTING NUMBERS TO 30
 	for (x = 1; x <= 30; x++)
 	{
+		//ARITHMETIC TO OUTPUT COUNTING NUMBERS TO 10
 		if (x <= 10)
 		{
 			cout << x << ", ";
 		}
+		//ARITHMETIC TO OUTPUT COUNTING NUMBERS FROM 12 TO 30
 		if (x > 10 && x <= 30)
 		{
+			//FORMULA TO GET EVEN NUMBERS UP TO 30
 			if (x%2 == 0)
 			{
+				//FORMULA TO HAVE PERIOD AT THE END
 				if (x == 30)
 				{
 				cout << x << ".";
