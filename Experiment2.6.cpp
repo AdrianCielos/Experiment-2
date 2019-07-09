1731488e2811c702719b8d2c0da97635be3df39e
AdrianCielos <47727514+AdrianCielos@users.noreply.github.com>
2019-07-09 13:22:38 +0800

    Update Experiment2.6.cpp

--- Experiment2.6.cpp
+++ Experiment2.6.cpp
@@ -10,18 +10,21 @@ int main ()
 	cout << "Please Enter a VALID Number only." << endl; 
 	cout << endl; 
  
+	//INPUT OF VARIABLE
 	int x, y; 
 	for (y =1; y != 0; y++) 
 	{	 
 	
 	cout << "Enter Desired Number: "; cin >> y;
 	
+	//IF STATEMENT TO END THE LOOP
 	if(y<=0)
 		{
 		cout << "POSITIVE INTEGERS ONLY! THANK YOU." << endl;
 		break;
 		}
-
+	
+	//ARITHMETIC TO GET THE SUMMATION OF THE VARIABLE
 	int z=0;
 	for (x =1 ; x <= y; x++) 
 		{ 
