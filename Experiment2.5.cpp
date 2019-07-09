7a5c595edbba9bb8323dc403cde74175881c2993
AdrianCielos <47727514+AdrianCielos@users.noreply.github.com>
2019-07-09 13:20:25 +0800

    Update Experiment2.5.cpp

--- Experiment2.5.cpp
+++ Experiment2.5.cpp
@@ -7,17 +7,18 @@ int main ()
 	cout << "*** FIBONACCI ***" << endl;
 	cout <<  endl << endl;
 	
+	//ARITHMETIC TO PRODUCE FIBONACCI NUMBERS
 	int a, b = 0, c =1, d;
 	cout << "Fibonacci Numbers: " << endl;
-	
 	for (a=0; a < 22; a++)
 	{
+		//PUT PERIOD AT THE END
 		if (b == 10946)
 		{
 			cout << b << ".";
 			break;
 		}
-		
+		//Fibonacci Sequence
 		cout << b << ", ";
 		d =  b+c;
 		b=c;
